59d3aa726765dc5c0498856d3918054ded637dfc
Viktoria Dolgolevets <vikadolgolevec@gmail.com>
2024-11-05 18:34:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import json
-number = imput("введите номер квалификации: ")
-numbers = number[:len{number} - 3]
-found = True
-with open("dump.json", 'r') as file:
+import json #подключение модуля
+number = input("введите номер квалификации: ")
+numbers = number [:len(number) - 3] #ввод номера
+found = True #счетчик
+with open('dump.json', 'r', encoding='utf-8') as file: #открывает файл
     data = json.load(file)
-    for item in data:
-        if item['model'] == "data.spec" and item['files']['code'] == numbers:
-            print(f"=============== Найдено ===============")
-            print(f"{item['code']} >> Специальность "{item['titel']}", ПТО")
-            print(f"{item['fields']} >> Квалификация "{item['code']}"")
-        if item['model'] == "data.spec" and item['files']['code'] == number:
-            print(f"=============== Не найдено ===============")
-if found:
-
+    for item in data: #просматривает файл и записывает 
+        if item['model'] == "data.specialty" and item["fields"]["code"] == numbers:
+            print("=============== Найдено ===============")
+            print(f'{numbers} >> Специальность "{item["fields"]["title"]}", {item["fields"]["c_type"]}')
+            found = False
+        if item['model'] == "data.skill" and item["fields"]["code"] == number:
+            print(f'{number} >> Специальность "{item["fields"]["title"]}"')
+if found: #выводится если не найдено
+    print ("=============== Не найдено ===============")
